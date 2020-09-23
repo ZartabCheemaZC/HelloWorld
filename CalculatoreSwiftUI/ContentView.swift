b98f3ef9df2910b4c9a8ee9d18ccaa2b9bacd224
Zartab Munawar <zartab@gems.techverx.com>
2020-09-23 11:43:17 +0500

    First test commit

---
+++ CalculatoreSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatoreSwiftUI
+//
+//  Created by Zartab Cheema on 27/08/2020.
+//  Copyright © 2020 eazy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
